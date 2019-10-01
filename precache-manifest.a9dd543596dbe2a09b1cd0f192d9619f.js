3ac6110be1ac91b5845d06deea40c60f245a4ca0
papanugget <evilgenius04@gmail.com>
2019-10-01 16:34:38 -0400

    Updates

---
+++ precache-manifest.a9dd543596dbe2a09b1cd0f192d9619f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a3102b0e8db370a6386892840ae3048",
+    "url": "/react-todov2/index.html"
+  },
+  {
+    "revision": "b11748f54e921392e121",
+    "url": "/react-todov2/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "61c30f981fb2a50f5e26",
+    "url": "/react-todov2/static/css/main.3bc514eb.chunk.css"
+  },
+  {
+    "revision": "b11748f54e921392e121",
+    "url": "/react-todov2/static/js/2.ef78bc45.chunk.js"
+  },
+  {
+    "revision": "61c30f981fb2a50f5e26",
+    "url": "/react-todov2/static/js/main.d831f4b5.chunk.js"
+  },
+  {
+    "revision": "f9e439949d958178f8f8",
+    "url": "/react-todov2/static/js/runtime-main.02bde1cd.js"
+  }
+]);
